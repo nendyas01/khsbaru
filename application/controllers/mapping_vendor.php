0c3ad271716159427c10e2a463aa835d535bd55c
fathimahazzahratul <fathimahazzahratul@apps.ipb.ac.id>
2022-03-14 08:39:06 +0700

    mengedit login

--- application/controllers/mapping_vendor.php
+++ application/controllers/mapping_vendor.php
@@ -10,6 +10,9 @@ class mapping_vendor extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if(!$this->session->userdata("username")){
+			redirect('login');
+		}
         $this->load->model('m_mapping_vendor');
     }
 
