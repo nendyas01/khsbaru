653f96373d73d22b5b9607b8de007bd506bfbb16
fathimahazzahratul <fathimahazzahratul@apps.ipb.ac.id>
2022-03-14 10:22:35 +0700

    menambahkan dashboard d tiap view

--- application/controllers/Registrasi.php
+++ application/controllers/Registrasi.php
@@ -5,9 +5,6 @@ class registrasi extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        if(!$this->session->userdata("username")){
-			redirect('login');
-		}
         $this->load->model('m_registrasi');
         $this->load->library('form_validation');
     }
