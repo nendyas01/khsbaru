90739b468d0e67995bd20de896df95f35c5dc351
nendyas01 <nndyshaharani@apps.ipb.ac.id>
2022-03-14 22:25:34 +0700

    SKRD dan Retribusi

--- application/controllers/Retribusi_submit.php
+++ application/controllers/Retribusi_submit.php
@@ -13,12 +13,16 @@ class Retribusi_submit extends CI_Controller
 
     public function index()
     {
-        $data = [
+        $table = 'tb_ijin';
+        $where = array(
+            'surat_ijin_no' => $this->input->post('var_no_surat_ptsp')
+        );
+        $data = array(
             'tgl_bayar_retribusi' => date('Y-m-d', strtotime($this->input->post('var_tgl_bayar_retribusi'))),
             'evidence' => $this->input->post('evidence_ret')
-        ];
-        $file_name = $_FILES['NamaFile']['name'];
-        $this->db->insert('tb_ijin', $data, $file_name);
+        );
+
+        $this->m_retribusi->Update($where, $data, $table);
         echo '<script language="javascript">alert("Penyerahan Dokumen Berhasil Ditambahkan")</script>';
         echo '<script language="javascript">window.location = "retribusi"</script>';
     }
--- application/controllers/Skrd_submit.php
+++ application/controllers/Skrd_submit.php
@@ -5,20 +5,25 @@ class Skrd_submit extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        if(!$this->session->userdata("username")){
-			redirect('login');
-		}
+        if (!$this->session->userdata("username")) {
+            redirect('login');
+        }
         $this->load->model('m_skrd');
     }
 
     public function index()
     {
-        $data = [
-            'surat_ijin_no' => $this->input->post('var_no_surat_ptsp'),
+        $table = 'tb_ijin';
+        $where = array(
+            'surat_ijin_no' => $this->input->post('var_no_surat_ptsp')
+        );
+
+        $data = array(
             'tgl_terbit_skrd' => date('Y-m-d', strtotime($this->input->post('var_tgl_terbit_skrd'))),
             'biaya_retribusi' => $this->input->post('var_biaya_retribusi')
-        ];
-        $this->db->insert('tb_ijin', $data);
+        );
+
+        $this->m_skrd->Update($where, $data, $table);
         echo '<script language="javascript">alert("Penyerahan Dokumen Berhasil Ditambahkan")</script>';
         echo '<script language="javascript">window.location = "skrd"</script>';
     }
