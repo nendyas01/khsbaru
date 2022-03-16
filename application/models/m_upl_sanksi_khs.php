12f52fec5c2a65bdc505471f14dbf10d8ca48305
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2022-03-16 13:39:51 +0700

    Upload Sanksi KHS

--- application/models/m_upl_sanksi_khs.php
+++ application/models/m_upl_sanksi_khs.php
@@ -4,9 +4,14 @@ class m_upl_sanksi_khs extends CI_Model
 {
     function search_spj($title)
     {
-        $this->db->like('id_sanksi_spj', $title, 'BOTH');
-        $this->db->order_by('id_sanksi_spj', 'ASC');
+        $this->db->like('no_pelanggaran', $title, 'BOTH');
+        $this->db->order_by('no_pelanggaran', 'ASC');
         $this->db->limit(10);
-        return $this->db->get('tb_sanksi_spj')->result();
+        return $this->db->get('tb_pelanggaran_khs')->result();
+    }
+    function Update($where, $data, $table)
+    {
+        $this->db->update($table, $data, $where);
+        return $this->db->affected_rows();
     }
 }
