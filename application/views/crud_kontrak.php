b8845d731b983862df1e41adb2550b360994d712
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2022-07-20 09:03:03 +0700

    Mengedit pendownloadan

--- application/views/crud_kontrak.php
+++ application/views/crud_kontrak.php
@@ -481,8 +481,29 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
 
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+
 <script>
     $(document).ready(function() {
         $('#example').DataTable();
+    });
+
+    $('#example').DataTable({
+        dom: 'lBfrtip',
+        buttons: [{
+                align: 'center',
+                oriented: 'potrait',
+                download: 'open',
+                widthX: '90px'
+            },
+            'excel', 'pdf', 'print'
+        ]
     });
 </script>
